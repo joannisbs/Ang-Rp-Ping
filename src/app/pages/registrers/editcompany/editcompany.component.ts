aaf0c877ce460c1d903849d0da72a5a177b1b2f8
suporte4 <suporte4@realponto.intranet>
2018-06-20 16:20:16 -0300

    Adcionado mais campos com validacao

--- src/app/pages/registrers/editcompany/editcompany.component.ts
+++ src/app/pages/registrers/editcompany/editcompany.component.ts
@@ -11,17 +11,13 @@ import { Component, OnInit } from '@angular/core';
 
 export class EditcompanyComponent implements OnInit {
 
-  public spanerror = '<span class=\'input-group-addon\'><img src=\'/assets/icons/xred.png\' alt=\'ERRO\' class=\'erroicon\'></span>';
-
   constructor() { }
 
   ngOnInit() {
   }
   onSubmit(form) {
     console.log(form);
-    // if (input_name.valid) {
-    //   this.spanerror = '(<span class=\'input-group-addon\'><img src=\'/assets/icons/xred.png\' alt=\'ERRO\' class=\'erroicon\'></span>)';
-    // }
+
   }
 
 
