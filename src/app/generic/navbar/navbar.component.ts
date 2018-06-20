7703d1090690da3fc77b348b2d79f4aeedc08601
suporte4 <suporte4@realponto.intranet>
2018-06-20 10:30:56 -0300

    alterado icone reposivo

--- src/app/generic/navbar/navbar.component.ts
+++ src/app/generic/navbar/navbar.component.ts
@@ -8,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
 export class NavbarComponent implements OnInit {
   public idNav = null;
   public user_name = 'João';
+  public url_icon = "/assets/icons/menuwhite.png";
+  url_icon2 = "/assets/icons/xwhite.png";
 
   constructor() { }
 
   ngOnInit() { }
+
+  MenuIcon() {
+    const temp = this.url_icon2;
+    this.url_icon2 = this.url_icon;
+    this.url_icon = temp;
+  }
 
   mouseLeave() {
     this.idNav = null;
