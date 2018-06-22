bc91a87885ed31db02078c3f9d6a251a566a5feb
suporte4 <suporte4@realponto.intranet>
2018-06-22 15:53:45 -0300

    create rep-form

---
+++ src/app/pages/registrers/components/rep-form/rep-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rep-form',
+  templateUrl: './rep-form.component.html',
+  styleUrls: ['./rep-form.component.css']
+})
+export class RepFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
