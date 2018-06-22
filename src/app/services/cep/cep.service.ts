0dc6df80c514e8d3f6e3daa9af394211b2de5cc2
suporte4 <suporte4@realponto.intranet>
2018-06-22 15:51:10 -0300

    cep service now working

--- src/app/services/cep/cep.service.ts
+++ src/app/services/cep/cep.service.ts
@@ -1,26 +1,19 @@
+import { Http } from '@angular/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/observable';
+import 'rxjs/add/operator/map';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CepService {
 
-  constructor() { }
+  constructor(private http: Http) { }
 
-
-
-consultaCep(cep) {
- return console.log(cep);
-  // if (this.formulario.controls['inputCep'].valid) {
-  //   // tslint:disable-next-line:no-var-keyword
-  //   var cep = this.formulario.controls['inputCep'].value;
-  //   cep = cep.replace('-', '');
-  //   console.log(cep);
-  //   this.http.get(`https://viacep.com.br/ws/${cep}/json`).pipe(
-  //     map(dados => this.prencherEnd(dados)));
-  //     // .subscribe(dados => this.prencherEnd(dados));
-  // }
-
-}
+  consultaCep(cep): Observable<any> {
+    return this.http
+      .get(`https://viacep.com.br/ws/${cep}/json`)
+      .map(res => res.json());
+  }
 
 }
--- src/app/services/services.module.ts
+++ src/app/services/services.module.ts
@@ -1,17 +1,14 @@
+import { HttpModule } from '@angular/http';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CepService } from './cep/cep.service';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    HttpModule
   ],
   declarations: [],
-  providers: [
-    CepService,
-  ],
-  exports: [
-    CepService,
-  ]
+  providers: [CepService],
 })
 export class ServicesModule { }
