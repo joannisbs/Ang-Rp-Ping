ddf9563be6b07eb01332205e54467715bcda7679
suporte4 <suporte4@realponto.intranet>
2018-06-25 15:07:00 -0300

    modularização

--- src/app/pages/registrers/components/address-form/address.interface.ts
+++ src/app/pages/registrers/components/address-form/address.interface.ts
@@ -1,38 +1,39 @@
 import { Validators } from '@angular/forms';
 
 export const addressFormGroup = {
-    cep: ['',
-        [Validators.required,
-        Validators.maxLength(45),
-        Validators.pattern('[0-9]{5}[-][0-9]{3}')]],
 
-      logradouro: ['',
-        [Validators.required,
-        Validators.maxLength(60)]],
+  end_cep: ['',
+    [Validators.required,
+    Validators.maxLength(45),
+    Validators.pattern('([0-9]{5}[-][0-9]{3})|([0-9]{8})')]],
 
-      numero: ['',
-        [Validators.required,
-        Validators.maxLength(6),
-        Validators.pattern('[0-9]+')]],
+  end_rua: ['',
+    [Validators.required,
+    Validators.maxLength(60)]],
 
-      complemento: ['',
-        [Validators.maxLength(45)]],
+  end_num: ['',
+    [Validators.required,
+    Validators.maxLength(6),
+    Validators.pattern('[0-9]+')]],
 
-      uf: ['',
-        [Validators.required,
-        Validators.maxLength(2),
-        Validators.pattern('[A-Z]+')]],
+  end_comp: ['',
+    [Validators.maxLength(45)]],
 
-      cidade: ['',
-        [Validators.required,
-        Validators.maxLength(45)]],
+  end_uf: ['',
+    [Validators.required,
+    Validators.maxLength(2),
+    Validators.pattern('[A-Z]+')]],
 
-      bairro: ['',
-        [Validators.required,
-        Validators.maxLength(45)]],
+  end_cidade: ['',
+    [Validators.required,
+    Validators.maxLength(45)]],
 
-      inputRef: ['',
-        [Validators.required,
-        Validators.maxLength(45)]],
- };
+  end_bairro: ['',
+    [Validators.required,
+    Validators.maxLength(45)]],
+
+  end_ref: ['',
+    [Validators.required,
+    Validators.maxLength(45)]],
+};
 
---
+++ src/app/pages/registrers/components/company-form/company.interface.ts
@@ -0,0 +1,51 @@
+import { Validators } from '@angular/forms';
+
+export const companyFormGroup = {
+
+    emp_nome: ['',
+        [Validators.required,
+        Validators.maxLength(12),
+        Validators.pattern('[A-Z].*')]],
+
+    empdata_cnpj: ['',
+        [Validators.required,
+        Validators.maxLength(18),
+        // tslint:disable-next-line:max-line-length
+        Validators.pattern('^([0-9]{2}[\.][0-9]{3}[\.][0-9]{3}[\/][0-9]{4}(-)[0-9]{2})$|'
+        + '^[0-9]{14}$|'
+        + '^([0-9]{3}[\.][0-9]{3}[\.][0-9]{3}(()|(-))[0-9]{2})$|'
+        + '^([0-9]{11})$')]],
+
+    empdata_resp: ['',
+        [Validators.required,
+        Validators.maxLength(45)]],
+
+    empdata_razao: ['',
+        [Validators.required,
+        Validators.maxLength(90)]],
+
+    empdata_email: ['',
+        [Validators.required,
+        Validators.maxLength(45),
+        Validators.pattern('[^@]+@[^@]+\.[a-zA-Z]{2,6}')]],
+
+    empdata_tel: ['',
+        [Validators.required,
+        Validators.maxLength(14),
+        Validators.pattern('[\(]?[0-9]{2}(( )|([\)])|()|(-))(([0-9]{4})|([0-9]{5}))(( )|(-)|)([0-9]{4})')]],
+
+    emp_tela: ['',
+        [Validators.required,
+        Validators.maxLength(1),
+        Validators.pattern('[1-2]')]],
+
+    emp_coluna: ['',
+        [Validators.required,
+        Validators.maxLength(2),
+        Validators.pattern('(10|[0-9])')]],
+
+    emp_linha: ['',
+        [Validators.required,
+        Validators.maxLength(2),
+        Validators.pattern('[0-9]+')]],
+};
