28bc6fb045052353a50fbb2d2345ea666339a5b9
suporte4 <suporte4@realponto.intranet>
2018-06-27 10:31:36 -0300

    clonado editrep para new rep

---
+++ src/app/pages/registrers/newrep/newrep.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { addressFormGroup } from '../components/address-form/address.interface';
+import { repFormGroup } from '../components/rep-form/rep.interface';
+
+
+@Component({
+  selector: 'app-newrep',
+  templateUrl: './newrep.component.html',
+  styleUrls: ['./newrep.component.css']
+})
+export class NewrepComponent implements OnInit {
+
+  public formulario: FormGroup;
+  public imagebutton = '/assets/icons/xred.png';
+  public showimg = false;
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+    this.formulario = this.formBuilder.group ({
+        ...repFormGroup,
+        ...addressFormGroup
+    });
+  }
+  onSubmit() {
+    console.log(this.formulario.value);
+  }
+  hideImage() {
+    this.showimg = false;
+  }
+  showImage(value) {
+    this.showimg = true;
+    if (value) {
+      this.imagebutton = '/assets/icons/certoverde.png';
+    } else {
+      this.imagebutton = '/assets/icons/xred.png';
+    }
+
+  }
+
+}
