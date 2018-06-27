18713cd2377a7887c2a2e7a89c5fc0cfeb0a6cd5
suporte4 <suporte4@realponto.intranet>
2018-06-27 10:30:57 -0300

    adcionado nova tela de cadastro de equipamento novo

---
+++ src/app/pages/registrers/newequip/newequip.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-newequip',
+  templateUrl: './newequip.component.html',
+  styleUrls: ['./newequip.component.css']
+})
+export class NewequipComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
