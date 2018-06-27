c72493c01fa87561f655a60c024a1b056adbac33
suporte4 <suporte4@realponto.intranet>
2018-06-27 10:29:33 -0300

    Adcionado form do equipamento

---
+++ src/app/pages/registrers/components/equip-form/equip-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-equip-form',
+  templateUrl: './equip-form.component.html',
+  styleUrls: ['./equip-form.component.css']
+})
+export class EquipFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
