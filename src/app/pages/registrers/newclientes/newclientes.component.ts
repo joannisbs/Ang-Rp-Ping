9ecc5169bec9c9576cd1da5f4c129a2fc4fc8fc1
suporte4 <suporte4@realponto.intranet>
2018-06-27 10:29:58 -0300

    Criado new cliente

---
+++ src/app/pages/registrers/newclientes/newclientes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-newclientes',
+  templateUrl: './newclientes.component.html',
+  styleUrls: ['./newclientes.component.css']
+})
+export class NewclientesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
