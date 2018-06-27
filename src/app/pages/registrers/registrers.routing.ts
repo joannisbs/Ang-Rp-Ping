f6c192b3825a71656d6d442c4ea71371f44880ac
suporte4 <suporte4@realponto.intranet>
2018-06-27 10:28:12 -0300

    Arrumado as rotas

--- src/app/pages/registrers/registrers.routing.ts
+++ src/app/pages/registrers/registrers.routing.ts
@@ -2,20 +2,28 @@ import { Routes, RouterModule } from '@angular/router';
 import { RegistrersComponent } from './registrers.components';
 import { EditcompanyComponent } from './editcompany/editcompany.component';
 import { EditrepComponent } from './editrep/editrep.component';
-import { CreateequipComponent } from './createequip/createequip.component';
 import { EditclientesComponent } from './editclientes/editclientes.component';
 import { EdituserComponent } from './edituser/edituser.component';
+import { NewrepComponent } from './newrep/newrep.component';
+import { NewcompanyComponent } from './newcompany/newcompany.component';
+import { NewclientesComponent } from './newclientes/newclientes.component';
+import { NewequipComponent } from './newequip/newequip.component';
 
 const routes: Routes = [
   {
     path: '',
     component: RegistrersComponent,
     children: [
-      { path: 'empresa', component: EditcompanyComponent },
-      { path: 'relogio', component: EditrepComponent },
-      { path: 'equipamento', component: CreateequipComponent },
-      { path: 'usuarios', component: EdituserComponent },
-      { path: 'clientes', component: EditclientesComponent },
+      { path: 'newempresa', component: NewcompanyComponent },
+      { path: 'editempresa', component: EditcompanyComponent },
+      { path: 'newrelogio', component: NewrepComponent },
+      { path: 'editrelogio', component: EditrepComponent },
+      { path: 'newequipamento', component: NewequipComponent },
+      { path: 'newusuarios', component: EdituserComponent },
+      { path: 'newclientes', component: NewclientesComponent },
+      { path: 'editequipamento', component: NewequipComponent },
+      { path: 'editusuarios', component: EdituserComponent },
+      { path: 'editclientes', component: EditclientesComponent },
     ]
   }
 ];
