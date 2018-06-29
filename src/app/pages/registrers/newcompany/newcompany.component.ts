d162a104c20c06475831e855dd902ad6511ef4be
suporte4 <suporte4@realponto.intranet>
2018-06-29 15:11:59 -0300

    add funcao de show + modal

--- src/app/pages/registrers/newcompany/newcompany.component.ts
+++ src/app/pages/registrers/newcompany/newcompany.component.ts
@@ -1,24 +1,24 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 import { companyFormGroup } from '../components/company-form/company.interface';
 import { addressFormGroup } from '../components/address-form/address.interface';
 
 import { CompanyService } from './../../../services/company/company.service';
+import { Subscription } from 'rxjs';
+
 
 @Component({
   selector: 'app-newcompany',
   templateUrl: './newcompany.component.html',
   styleUrls: ['./newcompany.component.css']
 })
-export class NewcompanyComponent implements OnInit {
+export class NewcompanyComponent implements OnInit, OnDestroy {
 
   public formulario: FormGroup;
-  public imagebutton = '/assets/icons/xred.png';
-  public showimg = false;
-  public forsub = false;
   public ocorreuSubmit = false;
-
+  public showModal = false;
+  private subcription: Subscription;
 
   constructor(
     private formBuilder: FormBuilder,
@@ -35,27 +35,36 @@ export class NewcompanyComponent implements OnInit {
     });
 
   }
-  showImage(value) {
-    this.showimg = true;
-    if (value) {
-      this.imagebutton = '/assets/icons/certoverde.png';
-    } else {
-      this.imagebutton = '/assets/icons/xred.png';
-    }
-  }
-  hideImage() {
-    this.showimg = false;
+  ClearForm() {
+    this.formulario.reset();
+    this.showModal = false;
   }
   onSubmit(company, valido) {
-    this.ocorreuSubmit = true;
+
+    // caso Válido realisar metodo de envio
     if (valido) {
       console.log(company);
-
+      this.ocorreuSubmit = false;
       this.companyService
       .cadastrarCompany(company)
-      .subscribe(res => console.log(res));
+      .subscribe(res => {
+        console.log(res);
+        this.showModal = true;
+      }
+      );
+
+    } else {
+      // Variavel que da o feedback do botão precionado para os componets do form
+      this.ocorreuSubmit = true;
+      scrollTo(0, 0);
 
     }
-  }
 
+}
+  ngOnDestroy () {
+    this.showModal = false;
+    if (this.subcription) {
+      this.subcription.unsubscribe();
+    }
+  }
 }
