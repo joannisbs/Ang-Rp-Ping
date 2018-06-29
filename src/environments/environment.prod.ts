675b711e071713ea92bc874f6141d2f1b44e060e
suporte4 <suporte4@realponto.intranet>
2018-06-29 15:12:11 -0300

    Acertado dbjason

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  api_url: 'http://localhost:3000'
+  api_url: 'http://192.168.0.150:3000'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  api_url: 'http://localhost:3000'
+  api_url: 'http://192.168.0.150:3000'
 };
 
 /*
