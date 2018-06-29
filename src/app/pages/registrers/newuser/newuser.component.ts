d9d8dfb5a15083237e6ba7dce67e6bb4041219fa
suporte4 <suporte4@realponto.intranet>
2018-06-29 08:09:25 -0300

    Novo tela de usuário

--- src/app/pages/registrers/newuser/newuser.component.ts
+++ src/app/pages/registrers/newuser/newuser.component.ts
@@ -1,4 +1,7 @@
+
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { UserComumFormGroup } from '../components/user-comum-form/user-comum.interface';
 
 @Component({
   selector: 'app-newuser',
@@ -7,9 +10,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewuserComponent implements OnInit {
 
-  constructor() { }
+  public formulario: FormGroup;
+  public imagebutton = '/assets/icons/xred.png';
+  public showimg = false;
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.formulario = this.formBuilder.group ({
+      ...UserComumFormGroup,
+    });
   }
+  onSubmit() {
+    console.log(this.formulario.value);
+  }
+  hideImage() {
+    this.showimg = false;
+  }
+  showImage(value) {
+    this.showimg = true;
+    if (value) {
+      this.imagebutton = '/assets/icons/certoverde.png';
+    } else {
+      this.imagebutton = '/assets/icons/xred.png';
+    }
 
+  }
 }
