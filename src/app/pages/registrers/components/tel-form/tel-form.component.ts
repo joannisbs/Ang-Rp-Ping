b2e4991f24a38a3520628452a7c5ef8ecd39d3ee
suporte4 <suporte4@realponto.intranet>
2018-07-02 16:36:54 -0300

    Construido Form Tel

--- src/app/pages/registrers/components/tel-form/tel-form.component.ts
+++ src/app/pages/registrers/components/tel-form/tel-form.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
+import { arrumaTelefone } from './../../../../generic/funcoes-genericas/arrumaTel.function';
 
 @Component({
   selector: 'app-tel-form',
@@ -7,9 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TelFormComponent implements OnInit {
 
+  @Input()
+  telForm;
+
+  @Input()
+  ocorreuSubmit;
+
   constructor() { }
 
   ngOnInit() {
-  }
 
+  }
+  showErrors(value) {
+    return this.telForm.controls[ value ].valid || !(this.telForm.controls[ value].touched || this.ocorreuSubmit);
+  }
+  arrumaTel(value , campo) {
+    const res = arrumaTelefone(value);
+    this.telForm.get(campo).patchValue(res);
+  }
 }
