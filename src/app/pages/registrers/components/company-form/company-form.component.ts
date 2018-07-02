fcd7087edc87892efc86221a20e0d01d6b042b4a
suporte4 <suporte4@realponto.intranet>
2018-07-02 13:15:20 -0300

    Colocado funcao arruma cnpj

--- src/app/pages/registrers/components/company-form/company-form.component.ts
+++ src/app/pages/registrers/components/company-form/company-form.component.ts
@@ -1,5 +1,6 @@
 import { Validators } from '@angular/forms';
 import { Component, OnInit, Input } from '@angular/core';
+import { arrumaCNPJouCpf } from './../../../../generic/funcoes-genericas/arrumaCPFouCNPJ.function';
 
 @Component({
   selector: 'app-company-form',
@@ -22,33 +23,9 @@ export class CompanyFormComponent implements OnInit {
   showErrors(value) {
     return this.companyForm.controls[ value ].valid || !(this.companyForm.controls[ value].touched || this.ocorreuSubmit);
   }
-  arrumaCNPJ(cpf_cnpj) {
-    cpf_cnpj = cpf_cnpj.replace(/\./g , '');
-    cpf_cnpj = cpf_cnpj.replace('\-', '');
-    cpf_cnpj = cpf_cnpj.replace('\/', '');
-    console.log(cpf_cnpj);
-    if (cpf_cnpj.length === 11) {
-      let cpf = cpf_cnpj.substring(0, 3);
-      cpf = cpf + '.';
-      cpf = cpf + cpf_cnpj.substring(3, 6);
-      cpf = cpf + '.';
-      cpf = cpf + cpf_cnpj.substring(6, 9);
-      cpf = cpf + '-';
-      cpf = cpf + cpf_cnpj.substring(9, 11);
-      this.companyForm.get('empdata_cnpj').patchValue(cpf);
-    }
-    if (cpf_cnpj.length === 14) {
-      let cnpj = cpf_cnpj.substring(0, 2);
-      cnpj = cnpj + '.';
-      cnpj = cnpj + cpf_cnpj.substring(2, 5);
-      cnpj = cnpj + '.';
-      cnpj = cnpj + cpf_cnpj.substring(5, 8);
-      cnpj = cnpj + '/';
-      cnpj = cnpj + cpf_cnpj.substring(8, 12);
-      cnpj = cnpj + '-';
-      cnpj = cnpj + cpf_cnpj.substring(12, 14);
-      this.companyForm.get('empdata_cnpj').patchValue(cnpj);
-    }
+ arrumaCNPJ(value, campo) {
+    const res = arrumaCNPJouCpf(value);
+    this.companyForm.get(campo).patchValue(res);
   }
   arrumaTel(tel) {
     tel = tel.replace(/\(/g, '');
