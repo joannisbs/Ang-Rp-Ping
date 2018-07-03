92b85219fa65363a2704ce8af1156ea5799c8edc
suporte4 <suporte4@realponto.intranet>
2018-07-03 17:15:22 -0300

    add pagina cadastro de módulo

---
+++ src/app/pages/registrers/newmodule/newmodule.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-newmodule',
+  templateUrl: './newmodule.component.html',
+  styleUrls: ['./newmodule.component.css']
+})
+export class NewmoduleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
