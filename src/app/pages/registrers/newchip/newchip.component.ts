5cd159a94041fd615d7b1a0ea4bed8c17be225fc
suporte4 <suporte4@realponto.intranet>
2018-07-04 09:53:01 -0300

    Criado página principal cadastro de chip

--- src/app/pages/registrers/newchip/newchip.component.ts
+++ src/app/pages/registrers/newchip/newchip.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-newchip',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewchipComponent implements OnInit {
 
-  constructor() { }
+  public formulario: FormGroup;
+  public ocorreuSubmit = false;
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.formulario = this.formBuilder.group ({
+      // ...moduleFormGroup,
+    });
   }
+  onSubmit() {
+    console.log(this.formulario.value);
+    this.ocorreuSubmit = true;
+  }
+
 
 }
