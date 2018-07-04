14042c86a7078500a41aa0e59419249795acbaa8
suporte4 <suporte4@realponto.intranet>
2018-07-04 11:32:21 -0300

    Criado formArray para adcionar multiplos chips

--- src/app/pages/registrers/newchip/newchip.component.ts
+++ src/app/pages/registrers/newchip/newchip.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, FormArray } from '@angular/forms';
 import { chipFormGroup } from '../components/chip-form/chip.interface';
 
 @Component({
@@ -11,18 +11,60 @@ export class NewchipComponent implements OnInit {
 
   public formulario: FormGroup;
   public ocorreuSubmit = false;
+  public ctl_lixo = 0;
+  public chips;
 
   constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
-    this.formulario = this.formBuilder.group ({
-       ...chipFormGroup,
+    this.formulario = this.formBuilder.group({
+      ...chipFormGroup,
+      chips: this.formBuilder.array([this.createItem()]),
     });
   }
   onSubmit() {
     console.log(this.formulario.value);
     this.ocorreuSubmit = true;
   }
+  formarray () {
+    return this.formulario.get('chips').controls;
+  }
+  addChip(): void {
+    scrollTo(0, 100000);
+    this.chips = this.formulario.get('chips') as FormArray;
+    this.chips.push(this.createItem());
+  }
+  showErrorsFormArray(value, index) {
 
+    const chips = this.formulario.get('chips') as FormArray;
 
+    return chips.at(index).get(value).valid || !(chips.at(index).get(value).touched || this.ocorreuSubmit);
+
+  }
+
+  removeChip(chip) {
+    this.chips = this.formulario.get('chips') as FormArray;
+    this.chips.removeAt(chip);
+  }
+  lixo(i) {
+    this.ctl_lixo = i;
+  }
+  createItem(): FormGroup {
+    return this.formBuilder.group({
+
+      chip_Numchip: ['',
+        [Validators.required,
+        Validators.maxLength(45)]],
+      chip_ip: ['',
+        [Validators.required,
+        Validators.maxLength(45)]],
+      chip_Operadora: ['',
+        [Validators.required,
+        Validators.maxLength(45)]],
+      chip_Data: ['',
+        [Validators.required,
+        Validators.maxLength(45)]],
+    });
+  }
 }
+
