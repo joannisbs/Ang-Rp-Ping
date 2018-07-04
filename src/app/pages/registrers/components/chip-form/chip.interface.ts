28f14d355b2ded6e96992f89124b36ffb7624f81
suporte4 <suporte4@realponto.intranet>
2018-07-04 11:31:55 -0300

    Alterado ChipForm

--- src/app/pages/registrers/components/chip-form/chip.interface.ts
+++ src/app/pages/registrers/components/chip-form/chip.interface.ts
@@ -6,4 +6,14 @@ export const chipFormGroup = {
     chip_Numchip: ['',
         [Validators.required,
         Validators.maxLength(45)]],
+    chip_ip: ['',
+        [Validators.required,
+        Validators.maxLength(45)]],
+    chip_Operadora: ['',
+        [Validators.required,
+        Validators.maxLength(45)]],
+    chip_Data: ['',
+        [Validators.required,
+        Validators.maxLength(45)]],
+
 };
