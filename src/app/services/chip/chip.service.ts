1e5726cbe10b365211df79aff6b55df34aedcaa5
suporte4 <suporte4@realponto.intranet>
2018-07-10 11:21:06 -0300

    Criação de serviço de envio de dados do chip para o beckend

---
+++ src/app/services/chip/chip.service.ts
@@ -0,0 +1,25 @@
+import { Http } from '@angular/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/observable';
+import 'rxjs/add/operator/map';
+
+import { environment } from '../../../environments/environment';
+
+import { ChipInteface } from '../../models/chip/chip';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ChipService {
+
+  private api = environment.api_url;
+
+  constructor(private http: Http) { }
+
+  cadastrarChip(chip): Observable<ChipInteface> {
+    return this.http
+      .post(`${this.api}/chip`, chip)
+      .map(res => res.json());
+  }
+
+}
