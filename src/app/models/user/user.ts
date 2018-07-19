ca150fcb164e5d2ccdbb1e8289e6e2c6adf02131
Joannis Real Ponto <fedoraisreal@localhost.localdomain>
2018-07-19 11:30:42 -0300

    Separado modelo de login para modelo de cadastro de usuarios

--- src/app/models/user/user.ts
+++ src/app/models/user/user.ts
@@ -1,5 +1,9 @@
 export class UserInteface {
     user_name: string;
     user_pass: string;
-    user_type?: string;
+}
+export class newUserInteface {
+    user_name: string;
+    user_pass: string;
+    user_type: string;
 }
