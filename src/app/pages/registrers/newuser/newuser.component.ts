7de69367090706629799b35f943f75af4d9cec3f
Joannis Real Ponto <fedoraisreal@localhost.localdomain>
2018-07-19 11:33:43 -0300

    Criado link de serviço para criação de usuário

--- src/app/pages/registrers/newuser/newuser.component.ts
+++ src/app/pages/registrers/newuser/newuser.component.ts
@@ -2,6 +2,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder } from '@angular/forms';
 import { UserComumFormGroup } from '../components/user-comum-form/user-comum.interface';
+import { NewUserService } from './../../../services/user/user.service';
+import { AuthService } from '../../../services/login/auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-newuser',
@@ -10,19 +13,44 @@ import { UserComumFormGroup } from '../components/user-comum-form/user-comum.int
 })
 export class NewuserComponent implements OnInit {
 
+  private newUserService: NewUserService;
+  private subcription: Subscription;
+
   public formulario: FormGroup;
   public imagebutton = '/assets/icons/xred.png';
   public ocorreuSubmit = false;
-  constructor(private formBuilder: FormBuilder) { }
+  
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthService) { }
 
   ngOnInit() {
     this.formulario = this.formBuilder.group ({
       ...UserComumFormGroup,
     });
   }
-  onSubmit() {
-    console.log(this.formulario.value);
-    this.ocorreuSubmit = true;
+  
+  onSubmit(usuario, valido) {
+
+    if (valido) {
+      console.log('Valido');
+      this.ocorreuSubmit = false;
+      const token = this.authService.getToken();
+      this.subcription = this.newUserService
+      .newUser(token,usuario)
+      .subscribe(res => {
+        console.log(res);
+        // this.showModal = true;
+      }
+      );
+    } else {
+      this.ocorreuSubmit = true;
+    }
+  }
+  ngOnDestroy() {
+    if (this.subcription) {
+      this.subcription.unsubscribe();
+    }
   }
 
 }
