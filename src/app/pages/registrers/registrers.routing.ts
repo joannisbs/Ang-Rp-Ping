a480125f05ea9ff83631c85fda1f38e0c2681f0f
Joannis Real Ponto <fedoraisreal@localhost.localdomain>
2018-07-20 11:39:16 -0300

    Padronização

--- src/app/pages/registrers/registrers.routing.ts
+++ src/app/pages/registrers/registrers.routing.ts
@@ -1,18 +1,25 @@
+// Imports do angular
+import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+// imports de components
 import { RegistrersComponent } from './registrers.components';
-import { ModuleWithProviders } from '@angular/core';
 
 
 const routes: Routes = [
   {
     path: '',
     component: RegistrersComponent,
-    children: [
-
-      { path: 'newusuarios', loadChildren: 'src/app/pages/registrers/newuser/newuser.module#NewUserModule'},
-      { path: 'newreq', loadChildren: 'src/app/pages/registrers/newrequisition/newrequisition.module#NewRequisitionModule'}
-    
+    children: 
+    [
+      {
+        path: 'newusuarios', 
+        loadChildren: 'src/app/pages/registrers/newuser/newuser.module#NewUserModule'
+      },
+      { 
+        path: 'newreq', 
+        loadChildren: 'src/app/pages/registrers/newrequisition/newrequisition.module#NewRequisitionModule'
+      }
     ]
   }
 ];
