09ba946cede85a2fa66ca06f1d5099482671f50a
Joannis Real Ponto <fedoraisreal@localhost.localdomain>
2018-07-20 11:48:52 -0300

    criado guarda de expedição

---
+++ src/app/routesguards/expedicao.guard.ts
@@ -0,0 +1,30 @@
+import { AuthService } from '../services/login/auth.service';
+import { Observable } from 'rxjs';
+import { routes } from '../app.routing';
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+@Injectable()
+export class ExpedicaoGuard implements CanActivate{
+
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable <boolean> | boolean{
+      if (this.authService.getNivel()===5){
+        return true;
+      }
+      return false;
+  }
+
+}
