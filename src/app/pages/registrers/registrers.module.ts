618b90ad5bbfd58e4aa3b89b81c2f5f86b4d4025
Joannis Real Ponto <fedoraisreal@localhost.localdomain>
2018-07-20 11:37:29 -0300

    Padronização

--- src/app/pages/registrers/registrers.module.ts
+++ src/app/pages/registrers/registrers.module.ts
@@ -1,28 +1,33 @@
+// imports angular
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
+// imports componets
 import { RegistrersComponent } from './registrers.components';
+
+// imports rotas
 import { routing } from './registrers.routing';
 
 
 @NgModule({
-  imports: [
+
+  imports: 
+  [
     CommonModule,
     FormsModule,
-    ReactiveFormsModule,
-    RouterModule,
     HttpModule,
+    ReactiveFormsModule,
+    
+    RouterModule,
     routing
-
   ],
-  declarations: [
+
+  declarations: 
+  [
     RegistrersComponent,
-   
-   ],
-   entryComponents: [
   ]
 
 })
