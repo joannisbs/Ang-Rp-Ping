a0c17df4e38f2648c5c3ddf61109ff1ecfd68ade
Joannis Real Ponto <fedoraisreal@localhost.localdomain>
2018-07-20 11:48:29 -0300

    criado guarda de modulo+projeto

---
+++ src/app/routesguards/modulo_projeto.guard.ts
@@ -0,0 +1,30 @@
+import { AuthService } from '../services/login/auth.service';
+import { Observable } from 'rxjs';
+import { routes } from '../app.routing';
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+@Injectable()
+export class ModuloProjetoGuard implements CanActivate{
+
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable <boolean> | boolean{
+      if (this.authService.getNivel()===2){
+        return true;
+      }
+      return false;
+  }
+
+}
