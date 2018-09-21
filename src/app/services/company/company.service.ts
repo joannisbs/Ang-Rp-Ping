c90d063210f992cbab7837eba8c980ead6a2b387
Joannis Real Ponto <fedoraisreal@localhost.localdomain>
2018-09-21 11:54:28 -0300

    criação do metodo cadastrar  empresa

--- src/app/services/company/company.service.ts
+++ src/app/services/company/company.service.ts
@@ -4,6 +4,10 @@ import { Observable } from 'rxjs';
 import 'rxjs/add/operator/map';
 
 import { environment } from '../../../environments/environment';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/login/auth.service';
+import { StandartResponseInterface } from 'src/app/models/standartResponse/standartResponse';
+import { LoginInteface } from 'src/app/models/login/login';
 
 @Injectable({
   providedIn: 'root'
@@ -12,12 +16,46 @@ export class CompanyService {
 
   private api = environment.api_url;
 
-  constructor(private http: Http) { }
+  constructor(  private http: Http,
+                private authService: AuthService,
+                private router: Router
+  ) { }
 
   cadastrarCompany(company): Observable<any> {
+    const token: LoginInteface = this.authService.GetToken();
     return this.http
-      .post(`${this.api}/company`, company)
+      .post(`${this.api}/api/user/NewCompany`, [ token, company ] )
       .map(res => res.json());
   }
 
+  ValidateSeccion ( response_server ){
+
+    switch ( response_server ){
+      
+      case 3: // sucesso
+        return true;
+      
+      case 0: 
+        alert("Ocorreu um erro desconhecido. ( ernum: 0 )");
+        break;
+      
+      case 2:
+        alert("Sessão inválida, por favor faça login novamente.");
+        this.router.navigate(['/login']);
+        break;
+      
+      case 1:
+        alert("Sua conta não tem permissão para realizar esta tarefa, \n por favor contate um administrador.");
+      
+      case 4: 
+        alert("Ocorreu um erro desconhecido. ( ernum: 4 )");
+        break;
+
+      default:
+        alert("Ocorreu um erro desconhecido. ( ernum: errofrontdefault " + response_server + " )");
+        break;
+    }
+    return false;
+  }
 }
+
