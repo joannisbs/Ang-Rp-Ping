d78fb92575fa6b1edf7d4b9d5a7d74864d8afc36
Joannis Real Ponto <fedoraisreal@localhost.localdomain>
2018-09-25 14:22:18 -0300

    Object empresa interface

---
+++ src/app/models/company/company.ts
@@ -0,0 +1,22 @@
+export class EmpresaInteface {
+    emp_nome        :string;
+    empdata_cnpj    :string;
+    empdata_email   :string;
+    empdata_razao   :string;
+    empdata_resp    :string;
+    empdata_tel     :string;
+
+    end_bairro      :string;
+    end_cep         :string;
+    end_cidade      :string;
+    end_comp        :string;
+    end_num         :string;
+    end_ref         :string;
+    end_rua         :string;
+    end_uf          :string;
+
+    cont_On         :string;   
+    cont_Tot        :string;   
+    emp_ativo       :boolean;
+}
+
